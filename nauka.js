f39ff1ae6d87460638697cd0bf37a5f3d4523d9d
Olivia Klepacka <olivia.klepacka@gmail.com>
2022-04-22 16:36:32 +0200

    organizing with BEM

--- nauka.js
+++ nauka.js
@@ -2,3 +2,6 @@ console.log("wtf?!");
 
 const firstName = "Olivia";
 console.log(`Cześć, jestem ${firstName}`);
+
+const p = document.querySelector('.notes__days--js');
+p.innerHTML = `Cześć, jestem ${firstName}`;
